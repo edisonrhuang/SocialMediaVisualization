e415f4ce0a10a67ebdd36472fb09d036e172e0a1
Arashi <47728260+iiStormBringer@users.noreply.github.com>
2023-04-18 15:39:16 -0400

    DataTest Class
    Quick test for the Data class

---
+++ src/prj5/DataTest.java
@@ -0,0 +1,51 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+package prj5;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Edison Huang
+ * @version 4.18.2023
+ *
+ */
+public class DataTest {
+
+    // Fields ------------------------------------------------------------------
+    
+    private Data data;
+    private Influencer influencer;
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        influencer = new Influencer("aafootball", "allaboutfootball",
+            "ES", "Sports", 22876452, 333, 4650272, 518, 170095);
+        data = new Data("January", influencer);
+    }
+
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        data = null;
+    }
+
+
+    @Test
+    public void testGetters() {
+        assertEquals("January", data.getMonth());
+        assertEquals(influencer, data.getInfluencer());
+    }
+
+}
