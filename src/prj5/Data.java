40523a8cfc9786227c5575fd1c4e9dbc830a4924
Arashi <47728260+iiStormBringer@users.noreply.github.com>
2023-04-18 14:09:21 -0400

    Data class implementation
    Data class that takes in a month and an influencer field.

---
+++ src/prj5/Data.java
@@ -0,0 +1,49 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+package prj5;
+
+/**
+ * @author Edison Huang
+ * @version 4.18.2023
+ *
+ */
+public class Data {
+    
+    // Fields ------------------------------------------------------------------
+    
+    private String month;
+    private Influencer influencer;
+    
+    // Constructors ------------------------------------------------------------
+    
+    /**
+     * Constructor for Data that takes in a month field and influencer field
+     * @param month Month of the data
+     * @param influencer Influencer field
+     */
+    public Data(String month, Influencer influencer) {
+        this.month = month;
+        this.influencer = influencer;
+    }
+    
+    // Methods -----------------------------------------------------------------
+    
+    /**
+     * Default getter for the month field
+     * @return Returns the month field
+     */
+    public String getMonth() {
+        return this.month;
+    }
+    
+    /**
+     * Default getter for the influencer field
+     * @return Returns the influencer
+     */
+    public Influencer getInfluencer() {
+        return this.influencer;
+    }
+}
--- src/prj5/ListInterface.java
+++ src/prj5/ListInterface.java
@@ -5,6 +5,28 @@ package prj5;
  *
  */
 public interface ListInterface<T> {
-
-    //hey edison
+    /**
+     * Adds the object to the end of the list.
+     *
+     * @precondition obj cannot be null
+     * @param obj
+     *            the object to add
+     * @throws IllegalArgumentException
+     *             if obj is null
+     */
+    public abstract void add(Influencer obj);
+    
+    /**
+     * returns the size
+     * 
+     * @return the size of the list
+     */
+    public abstract int size();
+    
+    /**
+     * returns whether the list is empty or not
+     * 
+     * @return true if empty false if not
+     */
+    public abstract boolean isEmpty();
 }
