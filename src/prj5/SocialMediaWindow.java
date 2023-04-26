7091e2d18a780e008417b2a9d50fd665ee6d0999
Arashi <47728260+iiStormBringer@users.noreply.github.com>
2023-04-25 21:39:23 -0400

    Fixed SocialMediaWindow
    Made it so that the first quarter sorts and data reading are actually reading the correct data

--- src/prj5/SocialMediaWindow.java
+++ src/prj5/SocialMediaWindow.java
@@ -18,6 +18,7 @@ public class SocialMediaWindow {
     private SLList[] listArr;
     private String rate;
     private SLList currData;
+    private SLList calculatedData;
 
     private static final int LEFTALIGN = 20;
     private static final int LEFTMIDALIGN = 100;
@@ -290,60 +291,279 @@ public class SocialMediaWindow {
         // bars[0].remove();
         // display information to gui
         Iterator<Data> pointer = currData.iterator();
-        System.out.println(currData.size());
         index = 0;
-        while (pointer.hasNext()) {
 
-            if (bars[index] != null)
-            {
-                window.removeShape(bars[index]);
+        if (currData.size() > 4) {
+            ArrayList<String> channels = new ArrayList<String>();
+            for (int x = 0; x < currData.size(); x++) {
+                // if the arraylist does not have channelname, add it
+                if (!channels.contains(currData.get(x).getInfluencer()
+                    .getChannelName())) {
+                    channels.add(currData.get(x).getInfluencer()
+                        .getChannelName());
+                }
             }
-            
-            // get random color for bars
-            TestableRandom generator = new TestableRandom();
-            color = new Color(generator.nextInt(255), generator.nextInt(255),
-                generator.nextInt(255));
 
-            curr = pointer.next().getInfluencer();
-
-            // update to appropriate name
-            channelNames[index].setText(curr.getChannelName());
-            window.addShape(channelNames[index]);
-
-// how to do the bars without a set height?
             if (rate.equals("T")) {
-                // update bars
-                bars[index] = new Shape(rateNums[index].getX(), 250 - (int)curr
-                    .getTraditionalEngagement(), 30, (int)curr
-                        .getTraditionalEngagement(), color);
-                window.addShape(bars[index]);
+                // make new list to store
+                SLList calculatedTrad = new SLList();
 
-                // update the rates
-                rateNums[index].setText(String.valueOf(df.format(curr
-                    .getTraditionalEngagement())));
-                window.addShape(rateNums[index]);
+                // loop through arraylist
+                for (int x = 0; x < channels.size(); x++) {
+                    // System.out.println(channels.get(x) + ": ");
+                    int likes = 0;
+                    int comments = 0;
+                    int follow = 0;
 
+                    // if the array list contains channelname and the month
+                    // equals
+                    // Jan-March add comments and likes to total comments
+                    // and likes
+                    for (int y = 0; y < currData.size(); y++) {
+                        if ((currData.get(y).getInfluencer().getChannelName()
+                            .equals(channels.get(x))) && (currData.get(y)
+                                .getMonth().equals("January") || currData.get(y)
+                                    .getMonth().equals("February") || currData
+                                        .get(y).getMonth().equals("March"))) {
+                            comments += currData.get(y).getInfluencer()
+                                .getComments();
+                            likes += currData.get(y).getInfluencer().getLikes();
+                        }
+                        // add the followers for just march for traditional
+                        // calculation
+                        if ((currData.get(y).getInfluencer().getChannelName()
+                            .equals(channels.get(x))) && (currData.get(y)
+                                .getMonth().equals("March"))) {
+                            follow = currData.get(y).getInfluencer()
+                                .getFollowers();
+                        }
+                    }
+                    double trad;
+                    // account for zeros
+                    if (follow == 0) {
+                        trad = 0.0;
+                    }
+                    // else calculate the traditional rate
+                    else {
+                        trad = ((likes + comments) / (double)follow) * 100.0;
+                    }
+                    // make new influencer to store data traditional data
+                    // and calculate
+                    // the total traditional data
+                    Influencer currInf = new Influencer("", channels.get(x), "",
+                        "", 0, 0, 0, 0, 0);
+                    currInf.setTraditionalRate(trad);
+                    Data data = new Data("", currInf);
+
+                    if (calculatedData == null) {
+                        calculatedTrad.add(data);
+                    }
+                    else {
+                        calculatedData.get(x).getInfluencer()
+                            .setTraditionalRate(trad);
+                    }
+                }
+
+                if (calculatedData == null) {
+                    calculatedData = calculatedTrad;
+                }
+
+                // calculatedData.sortByName();
+
+                for (int x = 0; x < calculatedData.size(); x++) {
+                    if (bars[x] != null) {
+                        window.removeShape(bars[x]);
+                    }
+
+                    // get random color for bars
+                    TestableRandom generator = new TestableRandom();
+                    color = new Color(generator.nextInt(255), generator.nextInt(
+                        255), generator.nextInt(255));
+
+                    curr = pointer.next().getInfluencer();
+
+                    // update to appropriate name
+                    // channelNames[x].setText(curr.getChannelName());
+                    channelNames[x].setText(channels.get(x));
+                    window.addShape(channelNames[x]);
+
+                    bars[x] = new Shape(rateNums[x].getX(), 250
+                        - (int)calculatedData.get(x).getInfluencer()
+                            .getTraditionalEngagement(), 30, (int)calculatedData
+                                .get(x).getInfluencer()
+                                .getTraditionalEngagement(), color);
+                    window.addShape(bars[x]);
+
+                    if (calculatedData.get(x).getInfluencer()
+                        .getTraditionalEngagement() == 0.0) {
+                        rateNums[x].setText("N/A");
+                        window.addShape(rateNums[x]);
+                    }
+                    else {
+                        // update the rates
+                        rateNums[x].setText(String.valueOf(df.format(
+                            calculatedData.get(x).getInfluencer()
+                                .getTraditionalEngagement())));
+                        window.addShape(rateNums[x]);
+                    }
+                }
             }
             else {
-                // update bars
-                bars[index] = new Shape(rateNums[index].getX(), 250 - (int)curr
-                    .getEngagementReach(), 30, (int)curr
-                        .getEngagementReach(), color);
-                window.addShape(bars[index]);
-                // update the rates
-                rateNums[index].setText(String.valueOf(df.format(curr
-                    .getEngagementReach())));
-                window.addShape(rateNums[index]);
-            }
-            index++;
+                SLList calculatedEng = new SLList();
+                for (int x = 0; x < channels.size(); x++) {
+                    // System.out.println(channels.get(x) + ": ");
+                    int likes = 0;
+                    int comments = 0;
+                    int views = 0;
+                    // get the channel names and their corresponding data for
+                    // views,
+                    // comments, and likes
+                    for (int y = 0; y < currData.size(); y++) {
+                        if ((currData.get(y).getInfluencer().getChannelName()
+                            .equals(channels.get(x))) && (currData.get(y)
+                                .getMonth().equals("January") || currData.get(y)
+                                    .getMonth().equals("February") || currData
+                                        .get(y).getMonth().equals("March"))) {
+                            views += currData.get(y).getInfluencer().getViews();
+                            // System.out.println("Views:" +
+                            // list[3].get(y).getInfluencer().getViews());
+                            // System.out.println("Total Views: " + views);
+                            comments += currData.get(y).getInfluencer()
+                                .getComments();
+                            likes += currData.get(y).getInfluencer().getLikes();
+                        }
+                    }
+                    double reach;
+                    // account for zeros
+                    if (views == 0) {
+                        reach = 0.0;
+                    }
+                    // else calculate the reach
+                    else {
+                        reach = ((likes + comments) / (double)views) * 100.0;
+                    }
+                    // make influencer to store total reach data for each month
+                    Influencer currInf = new Influencer("", channels.get(x), "",
+                        "", 0, 0, 0, 0, 0);
+                    currInf.setEngagementRate(reach);
+                    Data data = new Data("", currInf);
+                    if (calculatedData == null) {
+                        calculatedEng.add(data);
+                    }
+                    else {
+                        calculatedData.get(x).getInfluencer().setEngagementRate(
+                            reach);
+                    }
+                }
 
+                if (calculatedData == null) {
+                    calculatedData = calculatedEng;
+                }
+
+                // sort list by engagement
+                calculatedData.sortByName();
+
+                for (int x = 0; x < calculatedData.size(); x++) {
+
+                    if (bars[x] != null) {
+                        window.removeShape(bars[x]);
+                    }
+
+                    // get random color for bars
+                    TestableRandom generator = new TestableRandom();
+                    color = new Color(generator.nextInt(255), generator.nextInt(
+                        255), generator.nextInt(255));
+
+                    curr = pointer.next().getInfluencer();
+
+                    // update to appropriate name
+                    // channelNames[x].setText(curr.getChannelName());
+                    channelNames[x].setText(channels.get(x));
+                    window.addShape(channelNames[x]);
+
+                    bars[x] = new Shape(rateNums[x].getX(), 250
+                        - (int)calculatedData.get(x).getInfluencer()
+                            .getEngagementReach(), 30, (int)calculatedData.get(
+                                x).getInfluencer().getEngagementReach(), color);
+                    window.addShape(bars[x]);
+
+                    if (calculatedData.get(x).getInfluencer()
+                        .getEngagementReach() == 0.0) {
+                        rateNums[x].setText("N/A");
+                        window.addShape(rateNums[x]);
+                    }
+                    else {
+                        // update the rates
+                        rateNums[x].setText(String.valueOf(df.format(
+                            calculatedData.get(x).getInfluencer()
+                                .getEngagementReach())));
+                        window.addShape(rateNums[x]);
+                    }
+                }
+            }
         }
-        
-        
+        else {
+            while (pointer.hasNext()) {
+                if (bars[index] != null) {
+                    window.removeShape(bars[index]);
+                }
+
+                // get random color for bars
+                TestableRandom generator = new TestableRandom();
+                color = new Color(generator.nextInt(255), generator.nextInt(
+                    255), generator.nextInt(255));
+
+                curr = pointer.next().getInfluencer();
+
+                // update to appropriate name
+                channelNames[index].setText(curr.getChannelName());
+                window.addShape(channelNames[index]);
+
+                // how to do the bars without a set height?
+                if (rate.equals("T")) {
+                    // update bars
+                    bars[index] = new Shape(rateNums[index].getX(), 250
+                        - (int)curr.getTraditionalEngagement(), 30, (int)curr
+                            .getTraditionalEngagement(), color);
+                    window.addShape(bars[index]);
+
+                    if (curr.getTraditionalEngagement() == 0.0) {
+                        rateNums[index].setText("N/A");
+                        window.addShape(rateNums[index]);
+                    }
+                    else {
+                        // update the rates
+                        rateNums[index].setText(String.valueOf(df.format(curr
+                            .getTraditionalEngagement())));
+                        window.addShape(rateNums[index]);
+                    }
+
+                }
+                else {
+                    // update bars
+                    bars[index] = new Shape(rateNums[index].getX(), 250
+                        - (int)curr.getEngagementReach(), 30, (int)curr
+                            .getEngagementReach(), color);
+                    window.addShape(bars[index]);
+
+                    if (curr.getEngagementReach() == 0.0) {
+                        rateNums[index].setText("N/A");
+                        window.addShape(rateNums[index]);
+                    }
+                    else {
+                        // update the rates
+                        rateNums[index].setText(String.valueOf(df.format(curr
+                            .getEngagementReach())));
+                        window.addShape(rateNums[index]);
+                    }
+                }
+                index++;
+            }
+        }
     }
-    
-    public void displayBars(Shape[] shape)
-    {
-        
+
+
+    public void displayBars(Shape[] shape) {
+
     }
 }
